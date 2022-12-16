ff48c0c3cad5a8b785a6ed9138e767506286f749
Agnar <105129747+agnar-nomad@users.noreply.github.com>
2022-12-16 22:25:00 +0100

    mobile menu done

--- frontend-practice/ableton-homepage-lvl1/script.js
+++ frontend-practice/ableton-homepage-lvl1/script.js
@@ -1,6 +1,10 @@
-const menuBtn = document.querySelector('.navbar-primary__btn-more');
-const menuPrimary = document.querySelector('.navbar-primary');
+const menuBtn = document.querySelector('.navbar-primary__btn-menu');
+const [logo1, logo2] = document.querySelectorAll('.logo');
+const menuPrimary = document.querySelector('.navbar-primary__content');
 
-menuBtn.addEventListener('click', () =>
-  menuPrimary.classList.toggle('is-expanded')
-);
+menuBtn.addEventListener('click', () => {
+  menuPrimary.classList.toggle('is-expanded');
+  logo1.classList.toggle('menu-is-down');
+  logo2.classList.toggle('menu-is-down');
+  menuBtn.classList.toggle('menu-is-down');
+});
