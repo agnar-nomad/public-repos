b0ad22b527c1d0c015bedd79cf55e43524adc383
Agnar <105129747+agnar-nomad@users.noreply.github.com>
2022-12-16 23:43:47 +0100

    responsive website and multi-level menu complete

--- frontend-practice/ableton-homepage-lvl1/script.js
+++ frontend-practice/ableton-homepage-lvl1/script.js
@@ -1,10 +1,23 @@
 const menuBtn = document.querySelector('.navbar-primary__btn-menu');
+const moreBtn = document.querySelector('.navbar-primary__btn-more');
 const [logo1, logo2] = document.querySelectorAll('.logo');
 const menuPrimary = document.querySelector('.navbar-primary__content');
+const menuMore = document.querySelector('.navbar-primary__more');
 
 menuBtn.addEventListener('click', () => {
   menuPrimary.classList.toggle('is-expanded');
   logo1.classList.toggle('menu-is-down');
   logo2.classList.toggle('menu-is-down');
   menuBtn.classList.toggle('menu-is-down');
+});
+
+moreBtn.addEventListener('click', () => {
+  menuMore.classList.toggle('more-is-open');
+  const extraHeight = menuMore.offsetHeight;
+  console.log(typeof extraHeight);
+  console.log(extraHeight);
+  console.log(menuMore);
+  document.querySelector('.navbar-secondary').style = `padding-top: ${
+    extraHeight + 20
+  }px`;
 });
